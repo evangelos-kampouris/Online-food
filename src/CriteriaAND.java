add4f3369f9d5775ced8701bfc4b63966984ce86
VKamp <you@example.com>
2025-03-30 19:22:44 +0300

    Patched unimplemented code to remove the errors
    Signed-off-by: VKamp <you@example.com>

--- src/CriteriaAND.java
+++ src/CriteriaAND.java
@@ -1,10 +1,10 @@
 import java.util.List;
 import java.util.Set;
 
-public class CriteriaAND implements Criteria {
+public class CriteriaAND{ //implements Criteria {
 
-    @Override
-    public Set<Shop> meetCriteria(Set<Shop> shops, Filtering filter) {
-        return List.of();
-    }
+//    @Override
+//    public Set<Shop> meetCriteria(Set<Shop> shops, Filtering filter) {
+//        return List.of();
+//    }
 }
--- src/CriteriaOR.java
+++ src/CriteriaOR.java
@@ -1,9 +1,9 @@
 import java.util.List;
 import java.util.Set;
 
-public class CriteriaOR implements Criteria {
-    @Override
-    public Set<Shop> meetCriteria(Set<Shop> shops, Filtering filter) {
-        return
-    }
+public class CriteriaOR{ //implements Criteria {
+//    @Override
+//    public Set<Shop> meetCriteria(Set<Shop> shops, Filtering filter) {
+//        return
+//    }
 }
