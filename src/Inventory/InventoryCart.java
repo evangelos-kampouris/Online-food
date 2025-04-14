dcf5644c419638daeecb08745dd2180ed0c6041f
VKamp <you@example.com>
2025-04-14 19:05:49 +0300

    updatecode() consideration TODO

--- src/Inventory/InventoryCart.java
+++ src/Inventory/InventoryCart.java
@@ -15,6 +15,8 @@ public class InventoryCart extends Inventory {
         }
         InventoryItem item = new InventoryItem(product, quantity);
         inventory.put(productName, item);
+
+        //TODO condider whether the cart must be update using updateCost()
     }
 
     @Override
