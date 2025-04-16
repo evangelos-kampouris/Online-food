616c302f92253f478aed72710f903405a7cd5637
VKamp <you@example.com>
2025-04-17 00:46:17 +0300

    Updated handler to support the streams correctly.

--- src/other/Handler.java
+++ src/other/Handler.java
@@ -25,60 +25,57 @@ public class Handler implements Runnable{
         }
     }
 
-
     /**
      * Holds the handling of different requests. Each handling request is a class.
      *
      */
     public void handle(Entity entity, Socket connection) {
         try {
-            ObjectInputStream objectInputStream = new ObjectInputStream(connection.getInputStream());
             Object receivedObject = in.readObject();                        //ένα από τις επιλογές του request.txt
 
-            if(receivedObject instanceof BuyRequestDTO buyRequestDTO){
+            if(receivedObject instanceof BuyRequestDTO DTO){
                 BuyRequestHandler buyRequestHandler = new BuyRequestHandler();
-                buyRequestHandler.handle(entity, connection, buyRequestDTO);
+                buyRequestHandler.handle(entity, connection, DTO, out, in);
             }
 
-            else if(receivedObject instanceof AddStoreRequestDTO){
-                AddStoreRequestDTO addStoreRequestDTO = (AddStoreRequestDTO) receivedObject;
+            else if(receivedObject instanceof AddStoreRequestDTO DTO){
                 AddStoreRequestHandler addStoreRequestHandler = new AddStoreRequestHandler();
-                addStoreRequestHandler.handle(entity, connection, addStoreRequestDTO);
+                addStoreRequestHandler.handle(entity, connection, DTO, out, in);
             }
 
-            else if(receivedObject instanceof ChangeStockDTO changeStockDTO){
+            else if(receivedObject instanceof ChangeStockDTO DTO){
                 ChangeStockHandler changeStockHandler = new ChangeStockHandler();
-                changeStockHandler.handle(entity, connection, changeStockDTO);
+                changeStockHandler.handle(entity, connection, DTO, out, in);
             }
 
-            else if(receivedObject instanceof FilterMapDTO filterMapDTO){
+            else if(receivedObject instanceof FilterMapDTO DTO){
                 FilterMapHandler filterMapHandler = new FilterMapHandler();
-                filterMapHandler.handle(entity, connection, filterMapDTO);
+                filterMapHandler.handle(entity, connection, DTO, out, in);
             }
 
-            else if(receivedObject instanceof MapResultDTO mapResultDTO){
+            else if(receivedObject instanceof MapResultDTO DTO){
                 MapResultHandler mapResultHandler = new MapResultHandler();
-                mapResultHandler.handle(entity, connection, mapResultDTO);
+                mapResultHandler.handle(entity, connection, DTO, out, in);
             }
 
-            else if(receivedObject instanceof ReducerResultDTO reducerResultDTO){
+            else if(receivedObject instanceof ReducerResultDTO DTO){
                 ReducerResultHandler reducerResultHandler = new ReducerResultHandler();
-                reducerResultHandler.handle(entity, connection, reducerResultDTO);
+                reducerResultHandler.handle(entity, connection, DTO, out, in);
             }
 
-            else if(receivedObject instanceof SearchRequestDTO searchRequestDTO){
+            else if(receivedObject instanceof SearchRequestDTO DTO){
                 SearchRequestHandler searchRequestHandler = new SearchRequestHandler();
-                searchRequestHandler.handle(entity, connection, searchRequestDTO);
+                searchRequestHandler.handle(entity, connection, DTO, out, in);
             }
 
-            else if(receivedObject instanceof StatsRequestDTO statsRequestDTO){
+            else if(receivedObject instanceof StatsRequestDTO DTO){
                 StatsRequestHandler statsRequestHandler = new StatsRequestHandler();
-                statsRequestHandler.handle(entity, connection, statsRequestDTO);
+                statsRequestHandler.handle(entity, connection, DTO, out, in);
             }
 
-            else if(receivedObject instanceof UpdateBuyDataDTO updateBuyDataDTO){
+            else if(receivedObject instanceof UpdateBuyDataDTO DTO){
                 UpdateBuyDataHandler updateBuyDataHandler = new UpdateBuyDataHandler();
-                updateBuyDataHandler.handle(entity, connection, updateBuyDataDTO);
+                updateBuyDataHandler.handle(entity, connection, DTO, out, in);
             }
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
@@ -90,6 +87,5 @@ public class Handler implements Runnable{
     @Override
     public void run() {                                         //θα τρέξει το νήμα εδώ
         handle(this.entity, this.connection);
-
     }
 }
