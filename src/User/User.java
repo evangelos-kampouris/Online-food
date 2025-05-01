17484033147247b98aa6c5ce8a5384ff84b5d090
VKamp <you@example.com>
2025-05-01 18:38:48 +0300

    Fixed single connections error

--- src/User/User.java
+++ src/User/User.java
@@ -1,6 +1,7 @@
 package User;
 
 import DTO.Request;
+import Responses.ResponseDTO;
 import other.Shop;
 
 import java.io.IOException;
@@ -37,9 +38,32 @@ public abstract class User {
 
     protected abstract void showMenu();
 
-    protected void sendRequest(Request request) throws IOException{
-        out.writeObject(request);
-        out.flush();
+    protected ResponseDTO<?> sendAndReceiveRequest(Request request){
+        ResponseDTO<?> response = null;
+        try {
+            connectionSocket = new Socket(MASTER_IP, MASTER_PORT);
+            out = new java.io.ObjectOutputStream(connectionSocket.getOutputStream());
+            in = new java.io.ObjectInputStream(connectionSocket.getInputStream());
+
+            System.out.println("Sending request to Port:" + connectionSocket.getPort());
+            out.writeObject(request);
+            out.flush();
+
+            //Wait and read response from server.
+            System.out.println("Awaiting response..."); //debug
+            response = (ResponseDTO<?>) in.readObject();
+            System.out.println("Response: " + response.isSuccess() + " " + response.getMessage()); //debug
+            System.out.println("Closing connection...");
+            closeConnection();
+
+            return response;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return new ResponseDTO<>(false, getClass() + "] An error occured either while sending or receiving a request.");
     }
 
     protected void closeConnection() throws IOException {
