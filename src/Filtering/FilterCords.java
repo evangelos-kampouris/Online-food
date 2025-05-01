74d9c9cff654bbf9254815dae03cc62f30079146
VKamp <you@example.com>
2025-05-01 12:44:18 +0300

    Overriden hash and equals

--- src/Filtering/FilterCords.java
+++ src/Filtering/FilterCords.java
@@ -4,6 +4,7 @@ import other.Coordinates;
 import java.io.Serial;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FilterCords implements Filtering {
     @Serial
@@ -23,6 +24,20 @@ public class FilterCords implements Filtering {
         filter.add(distance);
         filter.add(userCoordinates);
         return filter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // same reference
+        if (o == null || getClass() != o.getClass()) return false; // null or different class
+        FilterCords that = (FilterCords) o;
+        return Float.compare(that.distance, distance) == 0 &&
+                Objects.equals(userCoordinates, that.userCoordinates);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance, userCoordinates);
     }
 
 
