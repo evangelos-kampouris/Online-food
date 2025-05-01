e58db80bb7b262f54d734e05620201e8648cf3fe
VKamp <you@example.com>
2025-05-01 13:51:39 +0300

    Added serial Number

--- src/Filtering/Filtering.java
+++ src/Filtering/Filtering.java
@@ -1,8 +1,10 @@
 package Filtering;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public interface Filtering extends Serializable {
-
+    @Serial
+    static final long serialVersionUID = 1L;
     public Object getFilter();
 }
